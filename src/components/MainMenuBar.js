a74de9ab833b9276ea507c3a98f70ea21551478f
sniedzielski <sniedzielski@soldevelo.com>
2025-01-16 18:56:21 +0100

    OSB-11: added possibility to add whole new menu entry

--- src/components/MainMenuBar.js
+++ src/components/MainMenuBar.js
@@ -1,18 +1,52 @@
 import React, { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { useModulesManager } from "../helpers/modules";
+import MainMenuContribution from "./generics/MainMenuContribution";
 
-function getMenus(modulesManager, key) {
+function getMenus(modulesManager, key, rights) {
   const menus = modulesManager.getContribs(key);
   const menuConfig = modulesManager.getConf("fe-core", "menus", []);
   const isMenuConfigEmpty = !menuConfig || menuConfig.length === 0;
-  if ( !isMenuConfigEmpty ) {
+  
+  let processedMenus;
+  if (!isMenuConfigEmpty) {
     const sortedMenus = sortMenus(menus, menuConfig);
-    return processMenu(sortedMenus);
+    processedMenus = processMenu(sortedMenus);
+  } else {
+    processedMenus = processMenu(menus);
   }
-  else {
-    return processMenu(menus);
-  } 
+
+  const unmatchedMenus = getUnmatchedMenus(menuConfig, menus, rights, modulesManager);
+  return [...processedMenus, ...unmatchedMenus];
 };
+
+function getUnmatchedMenus(menuConfig, menus, rights, modulesManager) {
+  const existingIds = menus
+    .filter((menu) => typeof menu === 'object')
+    .map((menu) => menu.name);
+  const history = useHistory();
+  const unmatchedConfigs = menuConfig.filter((config) => !existingIds.includes(config.id));
+
+  return unmatchedConfigs
+  .map((config) => {
+    if (config.filter && !config.filter(rights)) {
+      return null;
+    }
+
+    return () => (
+      <MainMenuContribution
+        header={config.name}
+        menuId={config.id}
+        modulesManager={modulesManager}
+        rights={rights}
+        history={history}
+        entries={[]}
+      />
+    );
+  })
+  .filter(Boolean);
+}
 
 function processMenu(menus) {
   return menus
@@ -44,13 +78,14 @@ function sortMenus(menus, menuConfig) {
 
 const MainMenuBar = ({ children = null, contributionKey, reverse = false, ...delegated }) => {
   const modulesManager = useModulesManager();
+  const rights = useSelector((state) => state.core?.user?.i_user?.rights || []);
   const components = useMemo(() => {
-    const components = getMenus(modulesManager, contributionKey);
+    const components = getMenus(modulesManager, contributionKey, rights);
     if (reverse) {
       components.reverse();
     }
     return components;
-  }, [contributionKey, reverse]);
+  }, [contributionKey, reverse, rights]);
 
   return (
     <>
